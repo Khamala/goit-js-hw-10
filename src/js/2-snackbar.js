544e831790521aac98347a87acdd47e22b38c6a2
Khamala <elektrikamegawolt@gmail.com>
2024-04-14 15:47:17 +0200

    snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,13 +1,58 @@
-const isSuccess = true;
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve("Success! Value passed to resolve function");
-    } else {
-      reject("Error! Error passed to reject function");
+const formPromise = document.querySelector("form");
+formPromise.addEventListener("submit", handlerPromiseForm);
+
+function handlerPromiseForm(event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector("input[name='delay']");
+  const delay = parseInt(delayInput.value);
+
+  const stateRadiosBtn = document.getElementsByName("state");
+  let selectedState;
+
+  for (const radio of stateRadiosBtn) {
+    if (radio.checked) {
+      selectedState = radio.value;
+      break;
     }
-  }, 2000);
-});
+  }
 
-console.log(promise); // Об'єкт промісу
+  const generatorPromises = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (selectedState === "fulfilled") {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  generatorPromises
+    .then((delay) => {
+      iziToast.success({
+        title: "Ok",
+        titleColor: '#fff',
+        message: `Fulfilled promise in ${delay}ms`,
+        messageColor: '#fff',
+        position: "topRight",
+        backgroundColor: '#59A10D',
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: "Error",
+        titleColor: '#fff',
+        message: `Rejected promise in ${delay}ms`,
+        messageColor: '#fff',
+        position: "topRight",
+        backgroundColor: '#EF4040',
+      });
+    });
+
+    event.currentTarget.reset();
+}
+
+
